593228cff47a941d546c64a187f4376de848fcca
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-22 17:55:06 +0900

    feat(VendingMachine): 금액 추가와 잔돈 반환 기능 추가

--- study/src/main/java/com/vending/machine/study/VendingMachine.java
+++ study/src/main/java/com/vending/machine/study/VendingMachine.java
@@ -2,4 +2,13 @@ package com.vending.machine.study;
 
 public class VendingMachine {
 
+    private int change;
+
+    public void put(final int change) {
+        this.change += change;
+    }
+
+    public int getChanges() {
+        return change;
+    }
 }
