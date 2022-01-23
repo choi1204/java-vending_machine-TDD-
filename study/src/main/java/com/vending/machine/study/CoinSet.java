e27fb2b2a716eaf079a9e3a0c7c66cdf0425cd72
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-01-23 16:48:41 +0900

    Update CoinSet.java

--- study/src/main/java/com/vending/machine/study/CoinSet.java
+++ study/src/main/java/com/vending/machine/study/CoinSet.java
@@ -1,7 +1,9 @@
 package com.vending.machine.study;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public enum  CoinSet {
 
@@ -13,5 +15,17 @@ public enum  CoinSet {
         this.value = value;
     }
 
+    public static CoinSet valueOf(final int amount) {
+        return Arrays.stream(values())
+                .filter(coin -> coin.value == amount)
+                .findFirst()
+                .orElseThrow(()-> new IllegalArgumentException(amount + "원의 동전은 존재하지 않습니다."));
+    }
 
+    public static List<CoinSet> highestOrder() {
+        return Arrays.stream(values())
+                .sorted((o1, o2) -> Integer.compare(o2.value, o1.value))
+                .collect(Collectors.toList())
+                ;
+    }
 }
